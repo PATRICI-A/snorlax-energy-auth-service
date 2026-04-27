2e941f90b05d77b94a31439a46a82b8cb8340b96
sebastian <sebastian.castillejo@mail.escuelaing.edu.co>
2026-04-27 16:24:39 -0500

    scaffolding  inicial

---
+++ src/main/java/edu/eci/patricia/DOSW_patricia/DoswPatriciaApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.patricia.DOSW_patricia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswPatriciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswPatriciaApplication.class, args);
+	}
+
+}
